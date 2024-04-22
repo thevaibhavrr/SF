6f5325b5098dbb75127074f196905e55d0a55b8b
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-04-22 18:07:49 +0530

    update all product

--- src/components/products/allproduct.jsx
+++ src/components/products/allproduct.jsx
@@ -17,7 +17,7 @@ function Allproduct({ search, category, minPrice, maxPrice }) {
 
     const fetchProduct = async () => {
         try {
-            const response = await makeApi(`/api/get-all-products?name=${search}&category=${category}&minPrice=${minPrice}&maxPrice=${maxPrice}&page=${currentPage}`, "GET");
+            const response = await makeApi(`/api/get-all-products?name=${search}&category=${category}&minPrice=${minPrice}&maxPrice=${maxPrice}&page=${currentPage}&IsOutOfStock=false`, "GET");
             setProducts(response.data.products);
         setToalProduct(response.data.totalProducts);
 
