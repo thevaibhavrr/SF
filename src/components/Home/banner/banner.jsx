f3275f90ab329a459fdbd205d2144bb3e4e0ba58
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-05-01 11:54:33 +0530

    add banner

--- src/components/Home/banner/banner.jsx
+++ src/components/Home/banner/banner.jsx
@@ -1,16 +1,70 @@
-import React from 'react'
-import BannerImage from "../../../Images/Home/banner/home_banner.png"
+// import React from 'react'
+// import BannerImage from "../../../Images/Home/banner/home_banner.png"
+// import "../../../styles/Home/banner.css"
+// function HomeBanner() {
+//   return (
+//     <>
+//     <div className='Home_page_banner_main_div' >
+//         <img src={BannerImage} alt="Banner" className='home_page_banner_image' />
+//     </div>
+ 
+
+//     </>
+//   )
+// }
+
+// export default HomeBanner
+
+import React from "react";
+import { Carousel } from "react-bootstrap";
+// import "../../styles/Home/Banner.css";
 import "../../../styles/Home/banner.css"
-function HomeBanner() {
+import BannerImage from "../../../Images/Home/banner/home_banner.png"
+import BannerSecondImage from "../../../Images/Home/banner/banner_2.png"
+import BannerThirdImage from "../../../Images/Home/banner/banner_3.png"
+
+
+
+function Banner() {
+
+  const Banner = [
+    {
+      id: 1,
+      image: BannerImage
+    },
+    {
+      id: 2,
+      image: BannerSecondImage
+    },
+    {
+      id: 3,
+      image: BannerThirdImage
+    },
+   
+  ]
+
   return (
-    <>
-    <div className='Home_page_banner_main_div' >
-        <img src={BannerImage} alt="Banner" className='home_page_banner_image' />
+    <div className="main_banner_div">
+      <Carousel fade  >
+        {
+          Banner.map((item) => (
+            <Carousel.Item>
+              <img
+                key={item.id}
+                src={item.image}
+                alt="banner"
+                className="home_page_banner_image"
+                // initial={{ opacity: 0, scale: 0.5 , rotate: 0 }}
+                // animate={{ opacity: 1, scale: 1  , rotate: 20 }}
+                // transition={{ duration: 2.6 , repeatType: "reverse", repeat: Infinity}} 
+              />
+            </Carousel.Item>
+          ))
+        }
+
+      </Carousel>
     </div>
-
-
-    </>
-  )
+  );
 }
 
-export default HomeBanner
+export default Banner;
